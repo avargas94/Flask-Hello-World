06fe564aef66c7385a3d8bcf52a8c42711fc0796
Andrew Vargas <andrewvargas6933@gmail.com>
2018-04-04 17:45:02 -0500

    Completed Project

---
+++ hello_world.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from os import environ
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return 'Home Page'
+
+@app.route("/hello/<name>")
+def person(name):
+    html = """
+        <h1>
+            Hello {}!
+        </h1>
+        
+        <p>
+        How are you doing!
+        </p>
+    """
+    return html.format(name.title())
+
+@app.route("/jedi/<fname>/<lname>")
+def jedi(fname, lname):
+    name = lname[0:3] + fname[0:2]
+    return "Your Jedi Name is {}".format(name)
+
+
+if __name__ == "__main__":
+    app.run(host=environ['IP'],
+            port=int(environ['HOST']))
